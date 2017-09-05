1e769c27e10241e25ecaf77017d630022a890c6d
ALITAVASOL\Ali Tavasol <alitavasolpanahi@yahoo.com>
2017-09-05 20:18:00 +0430

    all

---
+++ Landship/Source/Landship/LandshipGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LandshipGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LANDSHIP_API ALandshipGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
