f585ac9b47ff6a488af4ea5639b1954e50761491
Sandallfon_sherkat <alitavasolpanahi@yahoo.com>
2018-02-24 15:34:00 +0330

    Things Are a little bit ok But Unreal May crash again.

---
+++ Landship/Source/Landship/Public/TankTrack1.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack1.generated.h"
+
+/**
+ * this is to set the maximum driving Force. and apply Forces to the scene.
+ */
+UCLASS(meta = (BlueprintSpawnableComponent))
+class LANDSHIP_API UTankTrack1 : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
