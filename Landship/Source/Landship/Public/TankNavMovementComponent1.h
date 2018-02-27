71db0f9be6810a07a8b129779f4ef2306b9f1119
Sandallfon_sherkat <alitavasolpanahi@yahoo.com>
2018-02-27 18:03:55 +0330

    DONO Whats Happening

---
+++ Landship/Source/Landship/Public/TankNavMovementComponent1.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/NavMovementComponent.h"
+#include "TankNavMovementComponent1.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LANDSHIP_API UTankNavMovementComponent1 : public UNavMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
